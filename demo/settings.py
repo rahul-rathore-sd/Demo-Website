4ad4dda83c7706736405d788dc5888ed28d4510f
rahul <rrathore1774@gmail.com>
2024-07-07 10:43:18 +0530

    done vercel settings

--- demo/settings.py
+++ demo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ntk==g&p1nl1(jy4=vl32jpqsbtj2f-8e%&9oi+$9*)j82gt(w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -126,3 +126,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles_build', 'static')
